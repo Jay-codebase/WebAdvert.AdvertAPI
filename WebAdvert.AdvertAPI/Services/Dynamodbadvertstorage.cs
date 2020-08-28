81432f29fd1e7d57122866663fadd36f51a5de59
Jayshree Suyog Navgale <Jayshree_Navgale@ad.infosys.com>
2020-08-28 22:19:03 +0530

    AdverApi microservice

---
+++ WebAdvert.AdvertAPI/Services/Dynamodbadvertstorage.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdvertApi.Models;
+using AutoMapper;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+
+namespace WebAdvert.AdvertAPI.Services
+{
+    public class Dynamodbadvertstorage : IAdvertStorageService
+    {
+        private readonly IMapper _mapper;
+        public Dynamodbadvertstorage(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+        public async Task<string> Add(AdvertModel model)
+        {
+            var dbmodel = _mapper.Map<AdvertdbModel> (model);
+            dbmodel.Id = new Guid().ToString();
+            dbmodel.CreationDateTime = DateTime.UtcNow;
+            dbmodel.Status = AdvertStatus.Pending;
+            using (var client= new AmazonDynamoDBClient())
+            {
+                using(var context = new DynamoDBContext(client))
+                {
+                    await context.SaveAsync(dbmodel);
+                }
+            }
+            return dbmodel.Id;
+        }
+
+        public async Task Confirm(ConfirmAdvertModel model)
+        {
+            using(var client = new AmazonDynamoDBClient())
+            {
+                using (var context = new DynamoDBContext(client))
+                {
+                    var record = await context.LoadAsync<AdvertdbModel>(model.Id);
+                    if (record == null)
+                    {
+                        throw new KeyNotFoundException($"A record with ID={model.Id} was mot found.");
+                    }
+                    if (model.Status == AdvertStatus.Active)
+                    {
+                        record.Status = AdvertStatus.Active;
+                        await context.SaveAsync(record);
+                    }
+                    else
+                    {
+                        await context.DeleteAsync(record);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ WebAdvert.AdvertAPI/Services/IAdvertStorageService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdvertApi.Models;
+
+namespace WebAdvert.AdvertAPI.Services
+{
+    public interface IAdvertStorageService
+    {
+        Task<string> Add(AdvertModel model);
+        Task Confirm(ConfirmAdvertModel model);
+    }
+}
